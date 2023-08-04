afa362d991dbc922c92ace627970a8d7cb20d425
Joaquin Rios <joaquin.rios@crecic.cl>
2023-08-03 22:56:47 -0400

    primer commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Page404 from './pages/Page404';
+
+function App() {
+  
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
+    </Router>
+  )
+  
+}
+
+export default App;
